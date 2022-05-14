c857a4b37e07996858a96a6bc2712c71c96b7e9a
KYG_Ideapad <masterchip@naver.com>
2022-05-14 16:28:46 +0900

    change name

--- BOJ/1021 회전하는 큐 v1.cpp
+++ BOJ/1021 회전하는 큐 v1.cpp
@@ -6,105 +6,43 @@
 #include <queue>
 using namespace std;
 
-int p1021()
-{    
+int p1021v2()
+{
+    // 한 방향으로 큐 회전하여 찾기
+    // 최소 델타 선택 (반대 방향 델타는 유도됨) 
+    
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    // get input
     int n, m;
     cin >> n >> m;
-    vector<int> input;
-    while (m--)
-    {
-        int x;
-        cin >> x;
-        input.push_back(x);
-    }
-
+    
     // init queue
     deque<int> q;
     for (int i = 0; i < n; ++i)
     {
         q.push_back(i + 1);
     }
-
-    auto getLeftDistance = [&q](const int x) -> int
-    {
-        int cnt = 1;
-        for (auto it = q.crbegin(); it != q.crend(); ++it)
-        {
-            if (*it == x)
-            {
-                return cnt;
-            }
-            else
-            {
-                ++cnt;
-            }
-        }
-
-        return -1;
-    };
-
-    auto getRightDistance = [&q](const int x) -> int
-    {
-        int cnt = 0;
-        for (auto it = q.cbegin(); it != q.cend(); ++it)
-        {
-            if (*it == x)
-            {
-                return cnt;
-            }
-            else
-            {
-                ++cnt;
-            }
-        }
-
-        return -1;
-    };
-
+    
     int answer = 0;
-    for (const int x : input)
+    for (int i = 0; i < m; ++i)
     {
-        // pop front
-        if (q.front() == x)
-        {
-            q.pop_front();
-        }
-        else
-        {
-            // find minimum distance (l/r)
-            int lcnt = getLeftDistance(x);
-            int rcnt = getRightDistance(x);
-            assert(lcnt != -1);
-            assert(rcnt != -1);
+        int x;
+        cin >> x;
 
-            if (lcnt < rcnt)
-            {
-                answer += lcnt;
-                while (lcnt--)
-                {
-                    q.push_front(q.back());
-                    q.pop_back();
-                }
-            }
-            else
-            {
-                answer += rcnt;
-                while (rcnt--)
-                {
-                    q.push_back(q.front());
-                    q.pop_front();
-                }
-            }
-
+        // rotate left
+        int lcnt = 0;
+        while (q.front() != x)
+        {
+            q.push_back(q.front());
             q.pop_front();
+            ++lcnt;
         }
+
+        int rcnt = q.size() - lcnt;
+        answer += min(lcnt, rcnt);
+        q.pop_front();
     }
 
     cout << answer << "\n";
-
     return 0;
 }
