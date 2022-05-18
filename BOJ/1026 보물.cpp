c380360a0dd0d091c0e53e20dfa2cd5633137b57
KYG_Ideapad <masterchip@naver.com>
2022-05-18 17:16:48 +0900

    1026

---
+++ BOJ/1026 보물.cpp
@@ -0,0 +1,28 @@
+// https://www.acmicpc.net/problem/1026
+// silver4
+// 2016.2.23
+
+#include <cstdio>
+#include <algorithm>
+const int MAX = 51;
+int main()
+{
+	int a[MAX], b[MAX], n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; ++i)
+		scanf("%d", &a[i]);
+	for (int i = 0; i < n; ++i)
+	{
+		scanf("%d", &b[i]); b[i] *= -1;
+	}
+
+	std::sort(a, a + n);
+	std::sort(b, b + n);
+
+	int sum = 0;
+	for (int i = 0; i < n; ++i)
+		sum += a[i] * -b[i];
+
+	printf("%d\n", sum);
+	return 0;
+}
