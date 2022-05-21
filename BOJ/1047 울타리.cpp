d73bf6f1475b82e7377e6c10e3700ff78fc17ce2
KYG_Ideapad <masterchip@naver.com>
2022-05-21 19:35:30 +0900

    1047

--- BOJ/1047 울타리.cpp
+++ BOJ/1047 울타리.cpp
@@ -8,42 +8,31 @@ using namespace std;
 
 int main()
 {
-	// 두개의 점으로 이뤄진 모든 직사각형 조합 (n^2)
+	// 4개의 점으로 이뤄진 모든 직사각형 조합 (n^4)
 	// 외부 나무 커트하고 내부 나무 순차로 커트하여 정답 확인
+	// 다른 방법: from border position -> stacked height, count (dp)
 
 	int n; // [2 40]
 	int x[40] = {};
 	int y[40] = {};
 	int h[40] = {};
-	vector<int> originTrees;
 	cin >> n;
 	for (int i = 0; i < n; ++i)
 	{
 		cin >> x[i] >> y[i] >> h[i];
-		originTrees.push_back(i);
 	}
 
+	// pick four points. it can be duplicated 
+	// so it's count range will be 1~4
 	int answer = 41;
 	for (int p0 = 0; p0 < n; ++p0)
 	{
-		vector<int> pvec; // point trees
-		pvec.push_back(p0);
-		for (int p1 = 0; p1 < n; ++p1)
+		for (int p1 = p0; p1 < n; ++p1)
 		{
-			if (find(pvec.begin(), pvec.end(), p1) != pvec.end()) continue;
-			pvec.push_back(p1);
-			for (int p2 = 0; p2 < n; ++p2)
+			for (int p2 = p1; p2 < n; ++p2)
 			{
-				if (find(pvec.begin(), pvec.end(), p2) != pvec.end()) continue;
-				pvec.push_back(p2);
-				for (int p3 = 0; p3 < n; ++p3)
+				for (int p3 = p2; p3 < n; ++p3)
 				{
-					if (find(pvec.begin(), pvec.end(), p3) != pvec.end()) continue;
-					pvec.push_back(p3);
-
-					// inside trees
-					vector<int> invec = originTrees;
-
 					// calc square border					
 					int minX = min({ x[p0], x[p1], x[p2], x[p3] });
 					int minY = min({ y[p0], y[p1], y[p2], y[p3] });
@@ -51,41 +40,36 @@ int main()
 					int maxY = max({ y[p0], y[p1], y[p2], y[p3] });
 					int multiX = maxY - minY == 0 ? 1 : 2;
 					int multiY = maxX - minX == 0 ? 1 : 2;
-					int bSize = (maxX - minX) * multiX + (maxY - minY) * multiY;
+					int bSize = (maxX - minX) * multiX + 
+								(maxY - minY) * multiY;
 					int cutCnt = 0;
 					int bGain = 0;
 
-					// cut over line trees
-					for (int i = 0; i < invec.size(); ++i)
+					// cut outside trees
+					vector<int> insideHs;
+					for (int i = 0; i < n; ++i)
 					{
-						int ti = invec[i];
-						if (x[ti] < minX || x[ti] > maxX ||
-							y[ti] < minY || y[ti] > maxY)
+						if (cutCnt >= answer) break;
+
+						if (x[i] < minX || x[i] > maxX ||
+							y[i] < minY || y[i] > maxY)
 						{
 							++cutCnt;
-							bGain += h[ti];
-							invec.erase(invec.begin() + i);
+							bGain += h[i];
 						}
-						
-						if (bGain >= bSize) break;
+						else insideHs.push_back(h[i]);
 					}
 
+					sort(insideHs.begin(), insideHs.end(), greater<int>());
+
 					// cut inside greedy
-					while (bGain < bSize && invec.size() > 0)
+					for (int i = 0; i < insideHs.size(); ++i)
 					{
-						int maxi = 0;
-						for (int i = 0; i < invec.size(); ++i)
-						{
-							int ti = invec[i];
-							if (h[ti] > h[invec[maxi]])
-							{
-								maxi = i;
-							}
-						}
+						if (bGain >= bSize) break;
+						if (cutCnt >= answer) break;
 
 						++cutCnt;
-						bGain += h[invec[maxi]];
-						invec.erase(invec.begin() + maxi);
+						bGain += insideHs[i];
 					}
 
 					if (bGain >= bSize && cutCnt < answer)
