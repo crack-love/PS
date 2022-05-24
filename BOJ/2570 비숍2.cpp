59186d5ad97bdaab736ffff8093a4374d2841157
KYG_Ideapad <masterchip@naver.com>
2022-05-25 01:42:33 +0900

    2570

--- BOJ/2570 비숍2.cpp
+++ BOJ/2570 비숍2.cpp
@@ -9,105 +9,108 @@ using namespace std;
 
 // ÀÌºÐ ¸ÅÄª (ÃÖ´ë ¸ÅÄª)
 
-unordered_set<int> edgeV0[8000];
-int visited[8000];
-int par[8000];
-
-bool tryLink(int x)
+struct p2570
 {
-	visited[x] = true;
+	unordered_set<int> edgeV0[8000];
+	int visited[8000];
+	int par[8000];
 
-	for (int v1 : edgeV0[x])
+	bool tryLink(int x)
 	{
-		int p = par[v1];
-		if (p == -1 || (!visited[p] && tryLink(p)))
+		visited[x] = true;
+
+		for (int v1 : edgeV0[x])
 		{
-			par[v1] = x;
-			return true;
-		}
-	}
-	return false;
-}
-
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-
-	int gridBlock[101][101] = {};
-	int n, m; // n[1 100], m[0 10000]
-	cin >> n >> m;
-	for (int i = 0; i < m; ++i)
-	{
-		int x, y;
-		cin >> x >> y;
-		gridBlock[x][y] = 1;
-	}
-
-	// init vertex
-	// v0/v1 index starts from one
-	int gridV0[101][101] = {};
-	int gridV1[101][101] = {};
-	int v0Cnt = 0;
-	int v1Cnt = 0;
-	for (int i = 1; i <= n; ++i)
-	{
-		for (int j = 1; j <= n; ++j)
-		{
-			if (gridBlock[i][j]) continue;
-
-			if (gridV0[i][j] == 0)
+			int p = par[v1];
+			if (p == -1 || (!visited[p] && tryLink(p)))
 			{
-				int nx = i;
-				int ny = j;
-				v0Cnt += 1;
-				while (nx <= n && ny <= n)
-				{
-					if (gridBlock[nx][ny]) break;
-					gridV0[nx++][ny++] = v0Cnt;
-				}
-			}
-
-			if (gridV1[i][j] == 0)
-			{
-				int nx = i;
-				int ny = j;
-				v1Cnt += 1;
-				while (nx <= n && ny >= 1)
-				{
-					if (gridBlock[nx][ny]) break;
-					gridV1[nx++][ny--] = v1Cnt;
-				}
+				par[v1] = x;
+				return true;
 			}
 		}
+		return false;
 	}
 
-	// init edge
-	for (int i = 1; i <= n; ++i)
+	int main()
 	{
-		for (int j = 1; j <= n; ++j)
+		ios::sync_with_stdio(false);
+		cin.tie(nullptr);
+
+		int gridBlock[101][101] = {};
+		int n, m; // n[1 100], m[0 10000]
+		cin >> n >> m;
+		for (int i = 0; i < m; ++i)
 		{
-			if (gridBlock[i][j]) continue;
-			int v0 = gridV0[i][j];
-			int v1 = gridV1[i][j];
-			edgeV0[v0].insert(v1);
+			int x, y;
+			cin >> x >> y;
+			gridBlock[x][y] = 1;
 		}
-	}
 
-	// bipariteMatching
-	int matchCnt = 0;
-	memset(par, -1, sizeof par);
-	for (int i = 1; i <= v0Cnt; ++i)
-	{
-		memset(visited, 0, sizeof visited);
-
-		if (tryLink(i))
+		// init vertex
+		// v0/v1 index starts from one
+		int gridV0[101][101] = {};
+		int gridV1[101][101] = {};
+		int v0Cnt = 0;
+		int v1Cnt = 0;
+		for (int i = 1; i <= n; ++i)
 		{
-			matchCnt += 1;
+			for (int j = 1; j <= n; ++j)
+			{
+				if (gridBlock[i][j]) continue;
+
+				if (gridV0[i][j] == 0)
+				{
+					int nx = i;
+					int ny = j;
+					v0Cnt += 1;
+					while (nx <= n && ny <= n)
+					{
+						if (gridBlock[nx][ny]) break;
+						gridV0[nx++][ny++] = v0Cnt;
+					}
+				}
+
+				if (gridV1[i][j] == 0)
+				{
+					int nx = i;
+					int ny = j;
+					v1Cnt += 1;
+					while (nx <= n && ny >= 1)
+					{
+						if (gridBlock[nx][ny]) break;
+						gridV1[nx++][ny--] = v1Cnt;
+					}
+				}
+			}
 		}
+
+		// init edge
+		for (int i = 1; i <= n; ++i)
+		{
+			for (int j = 1; j <= n; ++j)
+			{
+				if (gridBlock[i][j]) continue;
+				int v0 = gridV0[i][j];
+				int v1 = gridV1[i][j];
+				edgeV0[v0].insert(v1);
+			}
+		}
+
+		// bipariteMatching
+		int matchCnt = 0;
+		memset(par, -1, sizeof par);
+		for (int i = 1; i <= v0Cnt; ++i)
+		{
+			memset(visited, 0, sizeof visited);
+
+			if (tryLink(i))
+			{
+				matchCnt += 1;
+			}
+		}
+
+		cout << matchCnt;
+
+		return 0;
 	}
-
-	cout << matchCnt;
-
-	return 0;
-}
+};
