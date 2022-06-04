0d4f4d9382b7a6880dd2db782f1ced2f3728465a
KYG_Ideapad <masterchip@naver.com>
2022-06-04 16:35:12 +0900

    2512

---
+++ BOJ/2512 예산.cpp
@@ -0,0 +1,59 @@
+// https://www.acmicpc.net/problem/2512
+// silver3
+
+#include <iostream>
+using namespace std;
+
+#define max(a,b) (a<b?b:a)
+
+struct p2512
+{
+	int main()
+	{
+		ios::sync_with_stdio(0);
+		cin.tie(0);
+
+		int n, m;
+		int b[10000];
+		int bsum = 0;
+		int maxVal = 0;
+		cin >> n;
+		for (int i = 0; i < n; ++i)
+		{
+			cin >> b[i];
+			bsum += b[i];
+			maxVal = max(maxVal, b[i]);
+		}
+		cin >> m;
+
+		if (bsum <= m)
+		{
+			cout << maxVal;
+			return 0;
+		}
+
+		int l = 1;
+		int r = maxVal + 1;
+		while (l < r - 1)
+		{
+			int mid = (l + r) / 2;
+
+			int sum = 0;
+			for (int i = 0; i < n; ++i)
+			{
+				sum += b[i] < mid ? b[i] : mid;
+			}
+
+			if (sum > m)
+			{
+				r = mid;
+			}
+			else if (sum <= m)
+			{
+				l = mid;
+			}
+		}
+		cout << l;
+		return 0;
+	}
+};
