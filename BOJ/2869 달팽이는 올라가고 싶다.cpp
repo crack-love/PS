0a653f5869395c311c7c80fda5a87ce5f344d662
KYG_Ideapad <masterchip@naver.com>
2022-06-23 12:40:59 +0900

    2869

---
+++ BOJ/2869 달팽이는 올라가고 싶다.cpp
@@ -0,0 +1,34 @@
+// https://www.acmicpc.net/problem/2869
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int a, b, v;
+	cin >> a >> b >> v;
+
+	int dayMove = a - b;
+	int maxDay = v / dayMove + 1;
+	int disc = a / dayMove;
+	int minDay = maxDay - disc - 2;
+	while (minDay < maxDay)
+	{
+		if ((minDay - 1) * dayMove + a >= v)
+		{
+			break;
+		}
+		else
+		{
+			++minDay;
+		}
+	}
+	cout << minDay;
+
+	return 0;
+}
+
