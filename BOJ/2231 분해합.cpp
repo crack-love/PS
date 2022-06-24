4d9925d3cd18791f41bf08d9fdcd58a8ebeeece8
KYG_Ideapad <masterchip@naver.com>
2022-06-24 13:56:27 +0900

    2231

---
+++ BOJ/2231 분해합.cpp
@@ -0,0 +1,27 @@
+// https://www.acmicpc.net/problem/2231
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		int a = i;
+		int t = i;
+		while (t > 0)
+		{
+			a += t % 10;
+			t /= 10;
+		}
+		if (a == n)
+		{
+			cout << i;
+			return 0;
+		}
+	}
+	cout << 0;
+	return 0;
+}
