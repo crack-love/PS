b96450c0706b7da8d4a9813e5e739ad67bc133d1
KYG_Ideapad <masterchip@naver.com>
2022-07-16 23:13:40 +0900

    1719 dijk

--- BOJ/1719 택배.v1.dijk.cpp
+++ BOJ/1719 택배.v1.dijk.cpp
@@ -8,70 +8,73 @@
 #include <tuple>
 using namespace std;
 
-void dijk(int beg, int par[], vector<pair<int, int>> edge[])
+struct p1719_dijk
 {
-	int cost[201];
-	fill(cost, cost + 201, -1);
-
-	priority_queue<pair<int, int>> q;
-	q.push({0, beg});
-	cost[beg] = 0;
-
-	while (!q.empty())
+	void dijk(int beg, int par[], vector<pair<int, int>> edge[])
 	{
-		int c = -q.top().first;
-		int x = q.top().second; q.pop();
-		if (cost[x] != c) continue;
+		int cost[201];
+		fill(cost, cost + 201, -1);
 
-		for (pair<int, int>& e : edge[x])
+		priority_queue<pair<int, int>> q;
+		q.push({ 0, beg });
+		cost[beg] = 0;
+
+		while (!q.empty())
 		{
-			int nx = e.first;
-			int nc = c + e.second;
-			if (cost[nx] == -1 || nc < cost[nx])
+			int c = -q.top().first;
+			int x = q.top().second; q.pop();
+			if (cost[x] != c) continue;
+
+			for (pair<int, int>& e : edge[x])
 			{
-				cost[nx] = nc;
-				par[nx] = par[x] != -1 ? par[x] : nx;
-				q.push({-nc, nx});
+				int nx = e.first;
+				int nc = c + e.second;
+				if (cost[nx] == -1 || nc < cost[nx])
+				{
+					cost[nx] = nc;
+					par[nx] = par[x] != -1 ? par[x] : nx;
+					q.push({ -nc, nx });
+				}
 			}
 		}
 	}
-}
 
-int main()
-{
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-
-	vector<pair<int, int>> edge[201];
-	int par[201][201];
-	fill(&par[0][0], &par[200][201], -1);
-
-	int n, m;
-	cin >> n >> m;
-	for (int i = 0; i < m; ++i)
+	int main()
 	{
-		int a, b, c;
-		cin >> a >> b >> c;
-		edge[a].push_back({b, c});
-		edge[b].push_back({a, c});
-	}
+		ios::sync_with_stdio(0);
+		cin.tie(0);
+		cout.tie(0);
 
-	for (int i = 1; i <= n; ++i)
-	{
-		dijk(i, par[i], edge);
-	}
+		vector<pair<int, int>> edge[201];
+		int par[201][201];
+		fill(&par[0][0], &par[200][201], -1);
 
-	for (int i = 1; i <= n; ++i)
-	{
-		for (int j = 1; j <= n; ++j)
+		int n, m;
+		cin >> n >> m;
+		for (int i = 0; i < m; ++i)
 		{
-			if (i == j) cout << "- ";
-			else if (par[i][j] == -1) cout << j << " ";
-			else cout << par[i][j] << " ";
+			int a, b, c;
+			cin >> a >> b >> c;
+			edge[a].push_back({ b, c });
+			edge[b].push_back({ a, c });
 		}
-		cout << "\n";
-	}
 
-	return 0;
-}
+		for (int i = 1; i <= n; ++i)
+		{
+			dijk(i, par[i], edge);
+		}
+
+		for (int i = 1; i <= n; ++i)
+		{
+			for (int j = 1; j <= n; ++j)
+			{
+				if (i == j) cout << "- ";
+				else if (par[i][j] == -1) cout << j << " ";
+				else cout << par[i][j] << " ";
+			}
+			cout << "\n";
+		}
+
+		return 0;
+	}
+};
