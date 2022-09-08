7b601d21ce7a7d5ac06e71b74fc175d1b8590046
KYG_Ideapad <masterchip@naver.com>
2022-09-08 16:12:27 +0900

    1475

--- BOJ/_Template.cpp
+++ BOJ/_Template.cpp
@@ -2,15 +2,9 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-#define for1(i,r) for(int i=0;i<r;++i)
 #define for2(i,j,r,c) for(int i=0;i<r;++i)for(int j=0;j<c;++j)
 
 int main0()
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);	cout.tie(0);
-
-
-
 	return 0;
 }
