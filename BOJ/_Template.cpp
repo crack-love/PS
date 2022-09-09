eaf08b1ee38a816bdbce5d533e06bb4542e8785a
KYG_Ideapad <masterchip@naver.com>
2022-09-09 16:19:25 +0900

    2490

--- BOJ/_Template.cpp
+++ BOJ/_Template.cpp
@@ -1,8 +1,8 @@
 // https://www.acmicpc.net/problem/
 
 #include <bits/stdc++.h>
-using namespace std;
 #define for2(i,j,r,c) for(int i=0;i<r;++i)for(int j=0;j<c;++j)
+using namespace std;
 
 int main0()
 {
