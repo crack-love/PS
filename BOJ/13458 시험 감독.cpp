3dcec559e0fe24a720f3a425896d03c30496965c
KYG_Ideapad <masterchip@naver.com>
2022-09-10 23:28:39 +0900

    13458

---
+++ BOJ/13458 시험 감독.cpp
@@ -0,0 +1,32 @@
+// https://www.acmicpc.net/problem/13458
+
+#include <bits/stdc++.h>
+#define for2(i,j,r,c) for(int i=0;i<r;++i)for(int j=0;j<c;++j)
+using namespace std;
+
+int main()
+{
+	int n;
+	int a[(int)1e6];
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> a[i];
+	}
+
+	int b, c; // รั,บฮ
+	cin >> b >> c;
+
+	long long ans = n;
+	for (int i = 0; i < n; ++i)
+	{
+		a[i] -= b;
+		if (a[i] > 0)
+		{
+			ans += ceil(a[i] / (double)c);
+		}
+	}
+
+	cout << ans;
+	return 0;
+}
