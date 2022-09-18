46637df8cf1b0998b00bd7f772f96d249a05823a
KYG_Ideapad <masterchip@naver.com>
2022-09-18 16:23:16 +0900

    2583

--- BOJ/_Template.cpp
+++ BOJ/_Template.cpp
@@ -2,6 +2,7 @@
 
 #include <bits/stdc++.h>
 #define for2(i,j,r,c) for(int i=0;i<r;++i)for(int j=0;j<c;++j)
+#define step(i,j,r,c) if (i < 0 || j < 0 || i >= r || j >= c) continue
 using namespace std;
 
 int main0()
