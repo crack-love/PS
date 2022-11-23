21bca8455182b4e2ed65230fff24f4a6347d8091
KYG_Ideapad <masterchip@naver.com>
2022-11-23 16:59:38 +0900

    11728

---
+++ BOJ/11728 배열 합치기.cpp
@@ -0,0 +1,51 @@
+// https://www.acmicpc.net/problem/11728
+
+#include <bits/stdc++.h>
+#define for2(i,j,r,c) for(int i=0;i<r;++i)for(int j=0;j<c;++j)
+#define step(i,j,r,c) if (i < 0 || j < 0 || i >= r || j >= c) continue
+using namespace std;
+
+struct p11728
+{
+	int main()
+	{
+		ios::sync_with_stdio(0);
+		cin.tie(0); cout.tie(0);
+
+		int n, m;
+		cin >> n >> m;
+		const int size = (int)1e6 + 1;
+		int a[size];
+		int b[size];
+		for (int i = 0; i < n; ++i)
+		{
+			cin >> a[i];
+		}
+		for (int i = 0; i < m; ++i)
+		{
+			cin >> b[i];
+		}
+
+		a[n] = 1e9 + 1;
+		b[m] = 1e9 + 1;
+		sort(a, a + n);
+		sort(b, b + m);
+		int ai = 0;
+		int bi = 0;
+		while (ai + bi < n + m)
+		{
+			if (a[ai] <= b[bi])
+			{
+				cout << a[ai] << " ";
+				ai += 1;
+			}
+			else
+			{
+				cout << b[bi] << " ";
+				bi += 1;
+			}
+		}
+
+		return 0;
+	}
+};
