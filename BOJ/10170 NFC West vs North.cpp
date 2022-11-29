2ddaf818d244b356882bb8f272c79b11f3c04c36
KYG_Ideapad <masterchip@naver.com>
2022-11-29 16:07:45 +0900

    15829 10170

--- BOJ/10170 NFC West vs North.cpp
+++ BOJ/10170 NFC West vs North.cpp
@@ -5,54 +5,57 @@
 #define step(i,j,r,c) if (i < 0 || j < 0 || i >= r || j >= c) continue
 using namespace std;
 
-struct nfc
+struct p10170
 {
-	string n;
-	int w;
-	int l;
-	int t;
-};
-
-void print(string name, const vector<nfc>& n)
-{
-	cout << setw(15) << left << name 
-		<< setw(3) << left << "W"
-		<< setw(2) << right << "L" 
-		<< setw(3) << right << "T" << "\n";
-	cout << "-----------------------" << "\n";
-	for (int i = 0; i < n.size(); ++i)
+	struct nfc
 	{
-		cout << setw(15) << left << n[i].n
-			<< setw(3) << left << n[i].w
-			<< setw(2) << right << n[i].l
-			<< setw(3) << right << n[i].t;
-		cout << "\n";
+		string n;
+		int w;
+		int l;
+		int t;
+	};
+
+	void print(string name, const vector<nfc>& n)
+	{
+		cout << setw(15) << left << name
+			<< setw(3) << left << "W"
+			<< setw(2) << right << "L"
+			<< setw(3) << right << "T" << "\n";
+		cout << "-----------------------" << "\n";
+		for (int i = 0; i < n.size(); ++i)
+		{
+			cout << setw(15) << left << n[i].n
+				<< setw(3) << left << n[i].w
+				<< setw(2) << right << n[i].l
+				<< setw(3) << right << n[i].t;
+			cout << "\n";
+		}
 	}
-}
 
-int main()
-{
-	ios::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-
-	vector<nfc> west = 
+	int main()
 	{
-		{"Seattle", 13, 3, 0},
-		{"San Francisco", 12, 4, 0},
-		{"Arizona", 10, 6, 0},
-		{"St. Louis", 7, 9, 0},
-	};
-	vector<nfc> north = 
-	{
-		{"Green Bay", 8, 7, 1},
-		{"Chicago", 8, 8, 0},
-		{"Detroit", 7, 9, 0},
-		{"Minnesota", 5, 10, 1}
-	};
+		ios::sync_with_stdio(0);
+		cin.tie(0); cout.tie(0);
 
-	print("NFC West", west);
-	cout << "\n";
-	print("NFC North", north);
+		vector<nfc> west =
+		{
+			{"Seattle", 13, 3, 0},
+			{"San Francisco", 12, 4, 0},
+			{"Arizona", 10, 6, 0},
+			{"St. Louis", 7, 9, 0},
+		};
+		vector<nfc> north =
+		{
+			{"Green Bay", 8, 7, 1},
+			{"Chicago", 8, 8, 0},
+			{"Detroit", 7, 9, 0},
+			{"Minnesota", 5, 10, 1}
+		};
 
-	return 0;
-}
+		print("NFC West", west);
+		cout << "\n";
+		print("NFC North", north);
+
+		return 0;
+	}
+};
