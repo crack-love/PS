548011fbe947febfe7d1ac4f6536105cb08f9b5e
KYG_Ideapad <masterchip@naver.com>
2022-12-05 14:55:44 +0900

    1057

---
+++ BOJ/1057 토너먼트.cpp
@@ -0,0 +1,29 @@
+// https://www.acmicpc.net/problem/1057
+
+#include <bits/stdc++.h>
+#define for2(i,j,r,c) for(int i=0;i<r;++i)for(int j=0;j<c;++j)
+#define step(i,j,r,c) if (i < 0 || j < 0 || i >= r || j >= c) continue
+using namespace std;
+
+struct p1057
+{
+	int main()
+	{
+		ios::sync_with_stdio(0);
+		cin.tie(0); cout.tie(0);
+
+		int n, a, b;
+		cin >> n >> a >> b;
+		int answer = 0;
+		a -= 1;
+		b -= 1;
+		while (a != b)
+		{
+			a = a / 2;
+			b = b / 2;
+			answer += 1;
+		}
+		cout << answer;
+		return 0;
+	}
+};
