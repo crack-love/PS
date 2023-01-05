57aac2da3e7661f282987299ca81c930887afe39
KYG_Ideapad <masterchip@naver.com>
2023-01-05 20:39:42 +0900

    10867

---
+++ BOJ/10867 중복 빼고 정렬하기.cpp
@@ -0,0 +1,35 @@
+// https://www.acmicpc.net/problem/10867
+
+#include <bits/stdc++.h>
+#define for2(i,j,r,c) for(int i=0;i<r;++i)for(int j=0;j<c;++j)
+#define step(i,j,r,c) if (i < 0 || j < 0 || i >= r || j >= c) continue
+using namespace std;
+
+struct p10867
+{
+	int main()
+	{
+		ios::sync_with_stdio(0);
+		cin.tie(0); cout.tie(0);
+
+		int n;
+		cin >> n;
+		int a[2001]{};
+		for (int i = 0; i < n; ++i)
+		{
+			int x;
+			cin >> x;
+			a[x + 1000] = 1;
+		}
+
+		for (int i = 0; i <= 2000; ++i)
+		{
+			if (a[i] > 0)
+			{
+				cout << i - 1000 << " ";
+			}
+		}
+
+		return 0;
+	}
+};
