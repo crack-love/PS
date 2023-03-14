e2e2c9b682ef1e36a065b5f16eab27278e574a41
KYG_Ideapad <masterchip@naver.com>
2023-03-14 16:40:54 +0900

    1302

---
+++ BOJ/1302 베스트셀러.cpp
@@ -0,0 +1,53 @@
+// https://www.acmicpc.net/problem/1302
+
+#include <bits/stdc++.h>
+#define for1(i,r) for(int i=0;i<r;++i)
+#define for2(i,j,r,c) for(int i=0;i<r;++i)for(int j=0;j<c;++j)
+#define step(i,j,r,c) if (i < 0 || j < 0 || i >= r || j >= c) continue
+using namespace std;
+
+struct p1302
+{
+	int main()
+	{
+		ios::sync_with_stdio(0);
+		cin.tie(0); cout.tie(0);
+
+		int n;
+		cin >> n;
+		int maxCnt = -1;
+		string answer;
+		unordered_map<string, int> m;
+
+		for1(i, n)
+		{
+			string s;
+			cin >> s;
+			auto it = m.find(s);
+			int cnt = 0;
+			if (it == m.end())
+			{
+				m.insert(it, { s,0 });
+				cnt = 0;
+			}
+			else
+			{
+				it->second += 1;
+				cnt = it->second;
+			}
+
+			if (cnt > maxCnt)
+			{
+				maxCnt = cnt;
+				answer = s;
+			}
+			else if (cnt == maxCnt && s < answer)
+			{
+				answer = s;
+			}
+		}
+
+		cout << answer;
+		return 0;
+	}
+};
