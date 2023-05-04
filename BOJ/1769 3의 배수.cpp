14530f24b005f8cf73f4c4d21f39dfbb01231c3c
KYG_Ideapad <masterchip@naver.com>
2023-05-04 16:43:31 +0900

    1769

--- BOJ/1769 3의 배수.cpp
+++ BOJ/1769 3의 배수.cpp
@@ -9,122 +9,125 @@ const int dy[4] = { 1,-1,0,0 };
 
 using namespace std;
 
-class bdigit
+struct p1769
 {
-private:
-	// (int)123->(bd)321
-	vector<char> v;
-
-public:
-	bdigit() 
+	class bdigit
 	{
-	};
+	private:
+		// (int)123->(bd)321
+		vector<char> v;
 
-	bdigit(const string& src)
-	{
-		for (int i = src.size() - 1; i >= 0; --i)
+	public:
+		bdigit()
 		{
-			v.push_back(src[i]-'0');
-		}
+		};
 
-		Validate(0);
-	}
-
-	void add(int val)
-	{
-		int d = 0;
-
-		while (val > 0)
+		bdigit(const string& src)
 		{
-			while (d >= v.size())
-				v.push_back(0);
-
-			v[d++] += val % 10;
-			val /= 10;
-		}
-
-		Validate(0);
-	}
-
-	char operator[](int idx) const
-	{
-		return v[v.size() - idx - 1];
-	}
-
-	int size() const
-	{
-		return v.size();
-	}
-
-private:
-	void Validate(int beg)
-	{
-		for (int i = beg; i < v.size(); ++i)
-		{
-			if (v[i] >= 10)
+			for (int i = src.size() - 1; i >= 0; --i)
 			{
-				while (i + 1 >= v.size())
+				v.push_back(src[i] - '0');
+			}
+
+			Validate(0);
+		}
+
+		void add(int val)
+		{
+			int d = 0;
+
+			while (val > 0)
+			{
+				while (d >= v.size())
 					v.push_back(0);
 
-				v[i+1] += v[i]/10;
-				v[i] %= 10;
+				v[d++] += val % 10;
+				val /= 10;
 			}
+
+			Validate(0);
 		}
-		
-		for (int i = v.size() - 1; i >= 0; --i)
+
+		char operator[](int idx) const
 		{
-			if (v[i] == 0)
+			return v[v.size() - idx - 1];
+		}
+
+		int size() const
+		{
+			return v.size();
+		}
+
+	private:
+		void Validate(int beg)
+		{
+			for (int i = beg; i < v.size(); ++i)
 			{
-				v.pop_back();
+				if (v[i] >= 10)
+				{
+					while (i + 1 >= v.size())
+						v.push_back(0);
+
+					v[i + 1] += v[i] / 10;
+					v[i] %= 10;
+				}
 			}
-			else
+
+			for (int i = v.size() - 1; i >= 0; --i)
 			{
-				break;
+				if (v[i] == 0)
+				{
+					v.pop_back();
+				}
+				else
+				{
+					break;
+				}
 			}
 		}
-	}
-};
+	};
 
-int highLevel = 0;
+	int highLevel = 0;
 
-bool isTri(bdigit bd, int level)
-{
-	highLevel = max(highLevel, level);
-
-	bdigit s;
-	for1(i, bd.size())
+	bool isTri(bdigit bd, int level)
 	{
-		s.add(bd[i]);
+		highLevel = max(highLevel, level);
+
+		bdigit s;
+		for1(i, bd.size())
+		{
+			s.add(bd[i]);
+		}
+
+		if (s.size() <= 1)
+		{
+			return s[0] % 3 == 0;
+		}
+		else
+		{
+			return isTri(s, level + 1);
+		}
 	}
 
-	if (s.size() <= 1)
+	int main()
 	{
-		return s[0] % 3 == 0;
-	}
-	else
-	{
-		return isTri(s, level + 1);
-	}
-}
+		ios::sync_with_stdio(0);
+		cin.tie(0); cout.tie(0);
 
-int main()
-{
-	ios::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
+		string x;
+		cin >> x;
 
-	string x;
-	cin >> x;
+		if (x.size() <= 1)
+		{
+			cout << 0 << "\n";
+			cout << ((x[0] % 3 == 0) ? "YES" : "NO");
+			return 0;
+		}
 
-	if (x.size() <= 1)
-	{
-		cout << 0 << "\n";
-		cout << ((x[0] % 3 == 0) ? "YES" : "NO");
+		bool res = isTri(x, 1);
+		cout << highLevel << "\n";
+		cout << (res ? "YES" : "NO");
+
 		return 0;
 	}
-
-	bool res = isTri(x, 1);
-	cout << highLevel << "\n";
-	cout << (res ? "YES" : "NO");
-
-	return 0;
-}
+};
