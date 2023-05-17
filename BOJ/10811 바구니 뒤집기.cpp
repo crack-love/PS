08e2ae06e1bfb81e60f068b36ec9b1c1b8aafc8b
KYG_Ideapad <masterchip@naver.com>
2023-05-17 14:56:33 +0900

    10811

---
+++ BOJ/10811 바구니 뒤집기.cpp
@@ -0,0 +1,43 @@
+// https://www.acmicpc.net/problem/10811
+
+#include <bits/stdc++.h>
+#define for1(i,r) for(int i=0;i<r;++i)
+#define for2(i,j,r,c) for(int i=0;i<r;++i)for(int j=0;j<c;++j)
+#define step(i,j,r,c) if (i < 0 || j < 0 || i >= r || j >= c) continue
+const int dx[4] = { 0,0,1,-1 };
+const int dy[4] = { 1,-1,0,0 };
+using namespace std;
+
+struct p10811
+{
+	int main()
+	{
+		ios::sync_with_stdio(0);
+		cin.tie(0); cout.tie(0);
+
+		int n, m;
+		cin >> n >> m;
+		int a[101];
+		for1(i, n + 1)
+		{
+			a[i] = i;
+		}
+		for1(i, m)
+		{
+			int x, y;
+			cin >> x >> y;
+
+			int halfP1 = (y - x) / 2 + 1;
+			for1(j, halfP1)
+			{
+				swap(a[x + j], a[y - j]);
+			}
+		}
+		for1(i, n)
+		{
+			cout << a[i + 1] << " ";
+		}
+
+		return 0;
+	}
+};
