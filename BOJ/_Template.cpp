791d1aadb2e13595b501dd722cb5d34867af5e38
KYG_Ideapad <masterchip@naver.com>
2023-07-05 17:03:12 +0900

    endl

--- BOJ/_Template.cpp
+++ BOJ/_Template.cpp
@@ -4,6 +4,7 @@
 #define for1(i,r) for(int i=0;i<r;++i)
 #define for2(i,j,r,c) for(int i=0;i<r;++i)for(int j=0;j<c;++j)
 #define step(i,j,r,c) if (i < 0 || j < 0 || i >= r || j >= c) continue
+#define endl "\n"
 const int dx[4] = { 0,0,1,-1 }, dy[4] = { 1,-1,0,0 };
 using namespace std;
 
