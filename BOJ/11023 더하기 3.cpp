882810ef7efcd9542426c34d2968b3b5eeeab556
KYG_Ideapad <masterchip@naver.com>
2023-08-06 15:26:30 +0900

    11023

---
+++ BOJ/11023 더하기 3.cpp
@@ -0,0 +1,32 @@
+// https://www.acmicpc.net/problem/11023
+
+#include <bits/stdc++.h>
+#define for1(i,r) for(int i=0;i<r;++i)
+#define for2(i,j,r,c) for(int i=0;i<r;++i)for(int j=0;j<c;++j)
+#define step(i,j,r,c) if (i < 0 || j < 0 || i >= r || j >= c) continue
+const int dx[4] = { 0,0,1,-1 }, dy[4] = { 1,-1,0,0 };
+using namespace std;
+
+struct p11023
+{
+	int main()
+	{
+		ios::sync_with_stdio(0);
+		cin.tie(0); cout.tie(0);
+
+		string line;
+		getline(cin, line);
+		stringstream ss(line);
+
+		int s = 0;
+		while (!ss.eof())
+		{
+			int x;
+			ss >> x;
+			s += x;
+		}
+		cout << s;
+
+		return 0;
+	}
+};
