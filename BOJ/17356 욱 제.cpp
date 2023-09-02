9d21a3994a14581997968e3413bb23e8c31686d0
KYG_Ideapad <masterchip@naver.com>
2023-09-02 20:11:46 +0900

    17356

---
+++ BOJ/17356 욱 제.cpp
@@ -0,0 +1,27 @@
+// https://www.acmicpc.net/problem/17356
+
+#include <bits/stdc++.h>
+#define for1(i,r) for(int i=0;i<r;++i)
+#define for2(i,j,r,c) for(int i=0;i<r;++i)for(int j=0;j<c;++j)
+#define step(i,j,r,c) if (i < 0 || j < 0 || i >= r || j >= c) continue
+const int dx[4] = { 0,0,1,-1 }, dy[4] = { 1,-1,0,0 };
+using namespace std;
+
+struct p17356
+{
+	int main()
+	{
+		ios::sync_with_stdio(0);
+		cin.tie(0); cout.tie(0);
+
+		int a, b;
+		cin >> a >> b;
+		double m = (b - a) / (double)400;
+		double r = 1 / (1 + pow(10, m));
+		cout.precision(4);
+		cout.setf(ios::fixed);
+		cout << r;
+
+		return 0;
+	}
+};
