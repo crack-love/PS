aa9c73cb9ef7e014f3e5905996fff30e044c7494
KYG_Ideapad <masterchip@naver.com>
2023-09-19 16:42:13 +0900

    fastio, ll, pii

--- BOJ/_Template.cpp
+++ BOJ/_Template.cpp
@@ -1,16 +1,18 @@
 // https://www.acmicpc.net/problem/
 
 #include <bits/stdc++.h>
+#define ll long long
+#define pii pair<int,int>
 #define for1(i,r) for(int i=0;i<r;++i)
 #define for2(i,j,r,c) for(int i=0;i<r;++i)for(int j=0;j<c;++j)
 #define step(i,j,r,c) if (i < 0 || j < 0 || i >= r || j >= c) continue
-const int dx[4] = { 0,0,1,-1 }, dy[4] = { 1,-1,0,0 };
+#define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+const int dx[8] = { 0,0,1,-1,1,1,-1,-1 }, dy[8] = { 1,-1,0,0,1,-1,1,-1 };
 using namespace std;
 
 int main0()
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
+	fastio;
 
 	return 0;
 }
