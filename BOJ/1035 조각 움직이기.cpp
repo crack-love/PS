dc0aa5ea3492192dd823d8356b18e68e72666999
KYG_Ideapad <masterchip@naver.com>
2023-11-16 20:23:03 +0900

    1035

--- BOJ/1035 조각 움직이기.cpp
+++ BOJ/1035 조각 움직이기.cpp
@@ -1,18 +1,24 @@
-// https://www.acmicpc.net/problem/
+// https://www.acmicpc.net/problem/27960
 
 #include <bits/stdc++.h>
 #define for1(i,r) for(int i=0;i<r;++i)
 #define for2(i,j,r,c) for(int i=0;i<r;++i)for(int j=0;j<c;++j)
 #define step(i,j,r,c) if (i < 0 || j < 0 || i >= r || j >= c) continue
 #define fastio ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define x first
+#define y second
 #define all(x) x.begin(),x.end()
 typedef int64_t ll;
 const int dx[8] = { 0,0,1,-1,1,1,-1,-1 }, dy[8] = { 1,-1,0,0,1,-1,1,-1 };
 using namespace std;
 
-int main0()
+int main()
 {
 	fastio;
+
+	int a, b;
+	cin >> a >> b;
+	cout << (a ^ b);
 
 	return 0;
 }
