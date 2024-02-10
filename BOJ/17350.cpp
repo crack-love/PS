9af5a1d761c49771c75b4ba2a2de977fc63c1db0
KYG_Ideapad <masterchip@naver.com>
2024-02-10 16:12:30 +0900

    17350

---
+++ BOJ/17350.cpp
@@ -0,0 +1,35 @@
+// https://www.acmicpc.net/problem/17350
+
+#include <bits/stdc++.h>
+#define for1(i,r) for(int i=0;i<r;++i)
+#define for2(i,j,r,c) for(int i=0;i<r;++i)for(int j=0;j<c;++j)
+#define step(i,j,r,c) if (i<0||j<0||i>=r||j>= c) continue
+#define fastio ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define all(x) x.begin(),x.end()
+typedef int64_t ll;
+const int dx[8] = { 0,0,1,-1,1,1,-1,-1 }, dy[8] = { 1,-1,0,0,1,-1,1,-1 };
+using namespace std;
+
+struct p17350
+{
+	int main()
+	{
+		fastio;
+
+		int n;
+		cin >> n;
+		for1(i, n)
+		{
+			string s;
+			cin >> s;
+			if (s == "anj")
+			{
+				cout << "构具;";
+				return 0;
+			}
+		}
+
+		cout << "构具?";
+		return 0;
+	}
+};
