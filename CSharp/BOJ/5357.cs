9dcdb0ff4e79bee820ac70dd6f987e3c120190bb
KYG_Ideapad <masterchip@naver.com>
2024-02-23 16:54:48 +0900

    5357

---
+++ CSharp/BOJ/5357.cs
@@ -0,0 +1,24 @@
+﻿internal class P5357
+{
+    internal static void Main()
+    {
+        var sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        var sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        int t = int.Parse(sr.ReadLine());
+        while (t-- > 0)
+        {
+            char last = (char)0;
+            foreach(var c in sr.ReadLine())
+            {
+                if (c == last)
+                    continue;
+                sw.Write(c);
+                last = c;
+            }
+            sw.WriteLine();
+        }
+
+        sw.Flush();
+    }
+}
