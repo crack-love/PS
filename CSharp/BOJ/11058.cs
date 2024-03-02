8ff663f61e3b2daf5310efa9a8f5fe222ed4aea0
KYG_Ideapad <masterchip@naver.com>
2024-03-02 17:41:48 +0900

    11058

--- CSharp/BOJ/11058.cs
+++ CSharp/BOJ/11058.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Immutable;
-using System.Xml;
-
-namespace BOJ;
+﻿namespace BOJ;
 
 class P11058
 {
--- CSharp/BOJ/_template.cs
+++ CSharp/BOJ/_template.cs
@@ -1,21 +1,15 @@
-﻿using System.Collections.Immutable;
-
-namespace BOJ;
+﻿namespace BOJ;
 class P
 {
+    static void Main0() => new P().Main();
     StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
     StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
     int[] dx = new int[] { 0, -1, 0, 1, -1, -1, 1, 1, 0 };
     int[] dy = new int[] { -1, 0, 1, 0, -1, 1, 1, -1, 0 };
     bool OutOfBound(int r, int c, int x, int y) => x < 0 || x >= r || y < 0 || y >= c;
     string[] ReadLine() => sr.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
-    
-    static void Main0()
-    {
-        new P().Solve();
-    }
 
-    void Solve()
+    void Main()
     {
         sw.Flush();
     }
