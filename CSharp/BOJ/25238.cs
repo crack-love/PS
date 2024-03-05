cab00c954c8287f3d656ba955d47303187e8d8ae
KYG_Ideapad <masterchip@naver.com>
2024-03-05 18:30:40 +0900

    25238

--- CSharp/BOJ/25238.cs
+++ CSharp/BOJ/25238.cs
@@ -1,5 +1,5 @@
 ﻿namespace BOJ;
-class P
+class P25238
 {
     static StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
     static StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
@@ -8,9 +8,12 @@ class P
     static bool OutOfBound(int r, int c, int x, int y) => x < 0 || x >= r || y < 0 || y >= c;
     static string[] ReadLine() => sr.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-    static void Main()
+    static void Main0()
     {
-
+        int[] s = ReadLine().Select(int.Parse).ToArray();
+        int a = s[0], b = s[1];
+        double r = a * (100-b) / 100d;
+        sw.WriteLine(r >= 100 ? 0 : 1);
         sw.Flush();
     }
 }
