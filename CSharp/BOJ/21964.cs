c1fc8f0ab31602db7eaf577209f3e70eaf968836
KYG_Ideapad <masterchip@naver.com>
2024-03-06 17:02:51 +0900

    21964

--- CSharp/BOJ/21964.cs
+++ CSharp/BOJ/21964.cs
@@ -1,5 +1,5 @@
 ﻿namespace BOJ;
-class P
+class P21964
 {
     static StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
     static StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
@@ -10,7 +10,12 @@ class P
 
     static void Main0()
     {
-
+        int n = ReadLine().Select(int.Parse).First();
+        var s = sr.ReadLine();
+        for (int i = 0; i < 5; ++i)
+        {
+            sw.Write(s[n-5+i]);
+        }
         sw.Flush();
     }
 }
