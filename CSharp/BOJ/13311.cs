e7dc08bb967158c749af7a6b96f46f42b7372c40
KYG_Ideapad <masterchip@naver.com>
2024-03-16 17:18:33 +0900

    13311

---
+++ CSharp/BOJ/13311.cs
@@ -0,0 +1,27 @@
+﻿#pragma warning disable CS8602, CS8321
+namespace BOJ;
+static class P13311
+{
+    static int[] dx = { 0, -1, 0, 1, -1, -1, 1, 1, 0 };
+    static int[] dy = { -1, 0, 1, 0, -1, 1, 1, -1, 0 };
+    static bool step(int x, int y, int r, int c) => x < 0 || x >= r || y < 0 || y >= c;
+    static StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    static StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    static string[] readStr() => sr.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    static T read1<T>(Func<string, T> f) => f(sr.ReadLine());
+    static (T, T) read2<T>(Func<string, T> f) { var a = readStr(); return (f(a[0]), f(a[1])); }
+
+    static void Main0()
+    {
+        var (x,y) = read2(int.Parse);
+        if (y >= x)
+        {
+            sw.WriteLine(y - x);
+        }
+        else
+        {
+            sw.WriteLine(y + x);
+        }
+        sw.Flush();
+    }
+}
--- CSharp/BOJ/_template2.cs
+++ CSharp/BOJ/_template2.cs
@@ -1,14 +1,16 @@
 ﻿#pragma warning disable CS8602
 namespace BOJ;
-class P
+class P2
 {
-    static void Main0() => new P().Solve();
-    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
-    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    static void Main0() => new P2().Solve();
     int[] dx = { 0, -1, 0, 1, -1, -1, 1, 1, 0 };
     int[] dy = { -1, 0, 1, 0, -1, 1, 1, -1, 0 };
-    bool Step(int x, int y, int r, int c) => x < 0 || x >= r || y < 0 || y >= c;
-    string[] ReadSplit() => sr.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    bool step(int x, int y, int r, int c) => x < 0 || x >= r || y < 0 || y >= c;
+    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    string[] readStr() => sr.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    T read1<T>(Func<string, T> f) => f(sr.ReadLine());
+    (T, T) read2<T>(Func<string, T> f) { var a = readStr(); return (f(a[0]), f(a[1])); }
 
     void Solve()
     {
