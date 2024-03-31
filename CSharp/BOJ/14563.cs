3c94149b71450609e07d75a9531186f3eec5c12f
KYG_Ideapad <masterchip@naver.com>
2024-03-31 16:31:34 +0900

    14563

---
+++ CSharp/BOJ/14563.cs
@@ -0,0 +1,31 @@
+﻿namespace BOJ;
+class P14563
+{
+    static void Main() => new P14563().Solve();
+    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    string[] ReadSplit() => sr.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+    void Solve()
+    {
+        int t = int.Parse(sr.ReadLine());
+        var a = ReadSplit().Select(int.Parse).ToArray();
+        for (int i = 0; i < t; ++i)
+        {
+            int sum = 0;
+            for (int j = 1; j< a[i]; ++j)
+            {
+                if (a[i] % j == 0)
+                    sum += j;
+            }
+            if (sum > a[i])
+                sw.WriteLine("Abundant");
+            else if (sum == a[i])
+                sw.WriteLine("Perfect");
+            else
+                sw.WriteLine("Deficient");
+        }
+
+        sw.Flush();
+    }
+}
