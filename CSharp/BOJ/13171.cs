a03cb73181fed3ad0d6afe705a311baacff38668
KYG_Ideapad <masterchip@naver.com>
2024-04-01 18:26:28 +0900

    13171

---
+++ CSharp/BOJ/13171.cs
@@ -0,0 +1,26 @@
+﻿namespace BOJ;
+class P13171
+{
+    static void Main0() => new P13171().Solve();
+    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+
+    void Solve()
+    {
+        long a = long.Parse(sr.ReadLine());
+        long x = long.Parse(sr.ReadLine());
+        int M = 1000000007;
+        long sq = a % M;
+        long ans = 1;
+        while (x > 0)
+        {
+            if ((x & 1) > 0)
+                ans = ans * sq % M;
+            x >>= 1;
+            sq = sq * sq % M;
+        }
+
+        sw.WriteLine(ans);
+        sw.Flush();
+    }
+}
