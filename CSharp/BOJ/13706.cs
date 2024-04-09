ff62467dc89114e05388de215a2aae877ca6a6d2
KYG_Ideapad <masterchip@naver.com>
2024-04-09 17:13:30 +0900

    13706

---
+++ CSharp/BOJ/13706.cs
@@ -0,0 +1,37 @@
+﻿using System.Numerics;
+
+namespace BOJ;
+class P13706
+{
+    static void Main0() => new P13706().Solve();
+    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    string[] ReadSplit() => sr.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+    void Solve()
+    {
+        BigInteger n = BigInteger.Parse(sr.ReadLine());
+        BigInteger left = 1;
+        BigInteger right = n;
+        while (true)
+        {
+            BigInteger m = ((left + right) / 2);
+            BigInteger sq = m * m;
+            if (sq < n)
+            {
+                left = m + 1;
+            }
+            else if (sq > n)
+            {
+                right = m;
+            }
+            else
+            {
+                sw.WriteLine(m);
+                break;
+            }
+        }
+
+        sw.Flush();
+    }
+}
