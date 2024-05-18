0f4bb0fb0698f2c36cc026ea998a1160b5fc2d27
KYG_Ideapad <masterchip@naver.com>
2024-05-18 16:31:50 +0900

    2568

---
+++ CSharp/BOJ/2568.cs
@@ -0,0 +1,63 @@
+﻿namespace BOJ;
+class P2568
+{
+    static void Main0() => new P2568().Solve();
+    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    string[] seps = { " ", "\t", };
+    string ReadLineUntil() { string s; do { s = sr.ReadLine(); } while (s.Length <= 0); return s; }
+    string[] ReadSplit() => ReadLineUntil().Split(seps, StringSplitOptions.RemoveEmptyEntries);
+    T[] ReadArray<T>(Func<string, T> f) => ReadSplit().Select(f).ToArray();
+    T Read1<T>(Func<string, T> f) => f(ReadLineUntil());
+    (T, T) Read2<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1]); }
+    (T, T, T) Read3<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1], s[2]); }
+
+    void Solve()
+    {
+        var n = Read1(int.Parse);
+        var a = new (int a,int b)[n];
+        for (int i = 0; i < n; ++i)
+            a[i] = Read2(int.Parse);
+        Array.Sort(a);
+
+        var da = new int[n];
+        var db = new int[n];
+        var len = 0;
+        var r = new int[n];
+        for (int i = 0; i < n; ++i)
+        {
+            var bi = Array.BinarySearch(db, 0, len, a[i].b);
+            if (bi == ~len)
+            {
+                da[len] = a[i].a;
+                db[len] = a[i].b;
+                r[i] = len;
+                len += 1;
+            }
+            else if (bi < 0)
+            {
+                da[~bi] = a[i].a;
+                db[~bi] = a[i].b;
+                r[i] = ~bi;
+            }
+        }
+
+        var st = new Stack<int>();
+        var tlen = len-1;
+        for (int i = n-1; i >= 0; --i)
+        {
+            if (r[i] == tlen)
+            {
+                tlen -= 1;
+                continue;
+            }
+
+            st.Push(a[i].a);
+        }
+
+        sw.WriteLine(n - len);
+        while (st.Count > 0)
+            sw.WriteLine(st.Pop());
+        sw.Flush();
+    }
+}
--- CSharp/BOJ/_template2.cs
+++ CSharp/BOJ/_template2.cs
@@ -7,9 +7,9 @@ class P
     int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1, 0 };
     int[] dy = { -1, 1, 0, 0, -1, 1, 1, -1, 0 };
     bool Step(int x, int y, int r, int c) => x < 0 || x >= r || y < 0 || y >= c;
-    string[] seperators = { " ", "\t", };
     string ReadLineUntil() { string s; do { s = sr.ReadLine(); } while (s.Length <= 0); return s; }
-    string[] ReadSplit() => ReadLineUntil().Split(seperators, StringSplitOptions.RemoveEmptyEntries);
+    string[] seps = { " ", "\t", };
+    string[] ReadSplit() => ReadLineUntil().Split(seps, StringSplitOptions.RemoveEmptyEntries);
     T[] ReadArray<T>(Func<string, T> f) => ReadSplit().Select(f).ToArray();
     T Read1<T>(Func<string, T> f) => f(ReadLineUntil());
     (T, T) Read2<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1]); }
