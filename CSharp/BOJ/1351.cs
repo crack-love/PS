2000b125249cda8da26495ee5ddda8d021bbf4ab
KYG_Ideapad <masterchip@naver.com>
2024-06-23 11:37:20 +0900

    1351

---
+++ CSharp/BOJ/1351.cs
@@ -0,0 +1,36 @@
+﻿namespace BOJ;
+class P1351
+{
+    static void Main0() => new P1351().Solve();
+    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1, 0 };
+    int[] dy = { -1, 1, 0, 0, -1, 1, 1, -1, 0 };
+    bool Step(int x, int y, int r, int c) => x < 0 || x >= r || y < 0 || y >= c;
+    string ReadLineUntil() { string s; do { s = sr.ReadLine(); } while (s.Length <= 0); return s; }
+    string[] seps = { " ", "\t", };
+    string[] ReadSplit() => ReadLineUntil().Split(seps, StringSplitOptions.RemoveEmptyEntries);
+    T[] ReadArray<T>(Func<string, T> f) => ReadSplit().Select(f).ToArray();
+    T Read1<T>(Func<string, T> f) => f(ReadLineUntil());
+    (T, T) Read2<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1]); }
+    (T, T, T) Read3<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1], s[2]); }
+
+    void Solve()
+    {
+        var (n, p, q) = Read3(long.Parse);
+        var d = new Dictionary<long, long>();
+        d[0] = 1;
+
+        long get(long x)
+        {
+            if (!d.TryGetValue(x, out var v))
+            {
+                d[x] = v = get(x / p) + get(x / q);
+            }
+            return v;
+        }
+
+        sw.WriteLine(get(n));
+        sw.Flush();
+    }
+}
