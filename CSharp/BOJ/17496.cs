c5b784867a9f4cd533309925dbe84ad2d4983b17
KYG_Ideapad <masterchip@naver.com>
2024-06-28 18:50:10 +0900

    17496

---
+++ CSharp/BOJ/17496.cs
@@ -0,0 +1,25 @@
+﻿namespace BOJ;
+class P17496
+{
+    static void Main0() => new P17496().Solve();
+    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    string ReadLineUntil() { string s; do { s = sr.ReadLine(); } while (s.Length <= 0); return s; }
+    string[] seps = { " ", "\t", };
+    string[] ReadSplit() => ReadLineUntil().Split(seps, StringSplitOptions.RemoveEmptyEntries);
+    T[] ReadArray<T>(Func<string, T> f) => ReadSplit().Select(f).ToArray();
+    T Read1<T>(Func<string, T> f) => f(ReadLineUntil());
+    (T, T) Read2<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1]); }
+    (T, T, T) Read3<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1], s[2]); }
+
+    void Solve()
+    {
+        var a = ReadArray(int.Parse);
+        var (n, t, c, p) = (a[0], a[1], a[2], a[3]);
+
+        var ans = c * p * ((n - 1) / t);
+        sw.WriteLine(ans);
+
+        sw.Flush();
+    }
+}
