c6b40fa54ba8179846d17245bf779f857eca7bbd
KYG_Ideapad <masterchip@naver.com>
2024-07-01 15:18:50 +0900

    13277

---
+++ CSharp/BOJ/13277.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+
+namespace BOJ;
+class P13277
+{
+    static void Main0() => new P13277().Solve();
+    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1, 0 };
+    int[] dy = { -1, 1, 0, 0, -1, 1, 1, -1, 0 };
+    bool Step(int x, int y, int r, int c) => x < 0 || x >= r || y < 0 || y >= c;
+    string ReadLineUntil() { string s; do { s = sr.ReadLine(); } while (s.Length <= 0); return s; }
+    string[] seps = { " ", "\t", };
+    string[] ReadSplit() => ReadLineUntil().Split(seps, StringSplitOptions.RemoveEmptyEntries);
+    T[] ReadArray<T>(Func<string, T> f) => ReadSplit().Select(f).ToArray();
+    T Read1<T>(Func<string, T> f) => f(ReadLineUntil());
+    (T, T) Read2<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1]); }
+    (T, T, T) Read3<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1], s[2]); }
+
+    void Solve()
+    {
+        var s = ReadSplit();
+        var a = BigInteger.Parse(s[0]);
+        var b = BigInteger.Parse(s[1]);
+        var c = a * b;
+
+        sw.WriteLine(c);
+        sw.Flush();
+    }
+}
