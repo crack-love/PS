96c44e4731d31e4e52f8bcee0ce6e575e70532bb
KYG_Ideapad <masterchip@naver.com>
2024-07-05 15:03:47 +0900

    2393

---
+++ CSharp/BOJ/2393.cs
@@ -0,0 +1,35 @@
+﻿namespace BOJ;
+class P2393
+{
+    static void Main0() => new P2393().Solve();
+    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1, 0 };
+    int[] dy = { -1, 1, 0, 0, -1, 1, 1, -1, 0 };
+    bool Step(int x, int y, int r, int c) => x < 0 || x >= r || y < 0 || y >= c;
+    string ReadLineUntil() { string s; do { s = sr.ReadLine(); } while (s.Length <= 0); return s; }
+    string[] seps = { " ", "\t", };
+    string[] ReadSplit() => ReadLineUntil().Split(seps, StringSplitOptions.RemoveEmptyEntries);
+    T[] ReadArray<T>(Func<string, T> f) => ReadSplit().Select(f).ToArray();
+    T Read1<T>(Func<string, T> f) => f(ReadLineUntil());
+    (T, T) Read2<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1]); }
+    (T, T, T) Read3<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1], s[2]); }
+
+    void Solve()
+    {
+        sw.WriteLine(@"  ___  ___  ___");
+        sw.WriteLine(@"  | |__| |__| |");
+        sw.WriteLine(@"  |           |");
+        sw.WriteLine(@"   \_________/");
+        sw.WriteLine(@"    \_______/");
+        sw.WriteLine(@"     |     |");
+        sw.WriteLine(@"     |     |");
+        sw.WriteLine(@"     |     |");
+        sw.WriteLine(@"     |     |");
+        sw.WriteLine(@"     |_____|");
+        sw.WriteLine(@"  __/       \__");
+        sw.WriteLine(@" /             \");
+        sw.WriteLine(@"/_______________\");
+        sw.Flush();
+    }
+}
