d84a9c2ab151ee7afff5b66d9617e496f3dce665
KYG_Ideapad <masterchip@naver.com>
2024-07-15 16:02:17 +0900

    27889

---
+++ CSharp/BOJ/27889.cs
@@ -0,0 +1,29 @@
+﻿namespace BOJ;
+class P27889
+{
+    static void Main0() => new P27889().Solve();
+    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1, 0 };
+    int[] dy = { -1, 1, 0, 0, -1, 1, 1, -1, 0 };
+    bool Step(int x, int y, int r, int c) => x < 0 || x >= r || y < 0 || y >= c;
+    string ReadLineUntil() { string s; do { s = sr.ReadLine(); } while (s.Length <= 0); return s; }
+    string[] seps = { " ", "\t", };
+    string[] ReadSplit() => ReadLineUntil().Split(seps, StringSplitOptions.RemoveEmptyEntries);
+    T[] ReadArray<T>(Func<string, T> f) => ReadSplit().Select(f).ToArray();
+    T Read1<T>(Func<string, T> f) => f(ReadLineUntil());
+    (T, T) Read2<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1]); }
+    (T, T, T) Read3<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1], s[2]); }
+
+    void Solve()
+    {
+        var d = new Dictionary<string, string>();
+        d.Add("NLCS", "North London Collegiate School");
+        d.Add("BHA", "Branksome Hall Asia");
+        d.Add("KIS", "Korea International School");
+        d.Add("SJA", "St. Johnsbury Academy");
+        var s = ReadLineUntil();
+        sw.WriteLine(d[s]);
+        sw.Flush();
+    }
+}
