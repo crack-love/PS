1cc4f9501b4b6d38382a63dfee8a19679ebf3b48
KYG_Ideapad <masterchip@naver.com>
2024-07-21 15:56:17 +0900

    4299

---
+++ CSharp/BOJ/24736.cs
@@ -0,0 +1,29 @@
+﻿namespace BOJ;
+class P24736
+{
+    static void Main0() => new P24736().Solve();
+    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1, 0 };
+    int[] dy = { -1, 1, 0, 0, -1, 1, 1, -1, 0 };
+    bool Step(int x, int y, int r, int c) => x < 0 || x >= r || y < 0 || y >= c;
+    string ReadLineUntil() { string s; do { s = sr.ReadLine(); } while (s.Length <= 0); return s; }
+    string[] seps = { " ", "\t", };
+    string[] ReadSplit() => ReadLineUntil().Split(seps, StringSplitOptions.RemoveEmptyEntries);
+    T[] ReadArray<T>(Func<string, T> f) => ReadSplit().Select(f).ToArray();
+    T Read1<T>(Func<string, T> f) => f(ReadLineUntil());
+    (T, T) Read2<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1]); }
+    (T, T, T) Read3<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1], s[2]); }
+
+    void Solve()
+    {
+        for (int i = 0; i< 2; ++i)
+        {
+            var l = ReadSplit().Select(x=>int.Parse(x)).ToArray();
+            var (t, f, s, p, c) = (l[0], l[1], l[2], l[3], l[4]);
+            var sum = t * 6 + f * 3 + s * 2 + p + c * 2;
+            sw.WriteLine(sum);
+        }
+        sw.Flush();
+    }
+}
