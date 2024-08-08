7ddec3f2229d94cf1dfefb6ad015edb0c479e7b0
KYG_Ideapad <masterchip@naver.com>
2024-08-08 16:12:41 +0900

    17863

---
+++ CSharp/BOJ/17863.cs
@@ -0,0 +1,28 @@
+﻿namespace BOJ;
+class P17863
+{
+    static void Main0() => new P17863().Solve();
+    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1, 0 };
+    int[] dy = { -1, 1, 0, 0, -1, 1, 1, -1, 0 };
+    bool Step(int x, int y, int r, int c) => x < 0 || x >= r || y < 0 || y >= c;
+    string ReadLineUntil() { string s; do { s = sr.ReadLine(); } while (s.Length <= 0); return s; }
+    string[] seps = { " ", "\t", };
+    string[] ReadSplit() => ReadLineUntil().Split(seps, StringSplitOptions.RemoveEmptyEntries);
+    T[] ReadArray<T>(Func<string, T> f) => ReadSplit().Select(f).ToArray();
+    T Read1<T>(Func<string, T> f) => f(ReadLineUntil());
+    (T, T) Read2<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1]); }
+    (T, T, T) Read3<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1], s[2]); }
+
+    void Solve()
+    {
+        var a = ReadLineUntil();
+        if (int.Parse(a[0..3]) == 555)
+            sw.WriteLine("YES");
+        else
+            sw.WriteLine("NO");
+
+        sw.Flush();
+    }
+}
