5ada47ef97846eb34af6cedd54d3cf5e7399001a
KYG_Ideapad <masterchip@naver.com>
2024-08-12 14:11:28 +0900

    5928

---
+++ CSharp/BOJ/5928.cs
@@ -0,0 +1,29 @@
+﻿namespace BOJ;
+class P5928
+{
+    static void Main0() => new P5928().Solve();
+    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1, 0 };
+    int[] dy = { -1, 1, 0, 0, -1, 1, 1, -1, 0 };
+    bool Step(int x, int y, int r, int c) => x < 0 || x >= r || y < 0 || y >= c;
+    string ReadLineUntil() { string s; do { s = sr.ReadLine(); } while (s.Length <= 0); return s; }
+    string[] seps = { " ", "\t", };
+    string[] ReadSplit() => ReadLineUntil().Split(seps, StringSplitOptions.RemoveEmptyEntries);
+    T[] ReadArray<T>(Func<string, T> f) => ReadSplit().Select(f).ToArray();
+    T Read1<T>(Func<string, T> f) => f(ReadLineUntil());
+    (T, T) Read2<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1]); }
+    (T, T, T) Read3<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1], s[2]); }
+
+    void Solve()
+    {
+        var (d, h, m) = Read3(int.Parse);
+        var begmin = 11 * 1440 + 11 * 60 + 11;
+        var endmin = d * 1440 + h * 60 + m;
+        if (endmin < begmin)
+            sw.Write(-1);
+        else
+            sw.Write(endmin - begmin);
+        sw.Flush();
+    }
+}
