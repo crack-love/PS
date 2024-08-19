672e74bf0f985bf646be73a6365d17129da3b84d
KYG_Ideapad <masterchip@naver.com>
2024-08-19 15:12:19 +0900

    16017

---
+++ CSharp/BOJ/16017.cs
@@ -0,0 +1,32 @@
+﻿namespace BOJ;
+class P16017
+{
+    static void Main0() => new P16017().Solve();
+    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1, 0 };
+    int[] dy = { -1, 1, 0, 0, -1, 1, 1, -1, 0 };
+    bool Step(int x, int y, int r, int c) => x < 0 || x >= r || y < 0 || y >= c;
+    string ReadLineUntil() { string s; do { s = sr.ReadLine(); } while (s.Length <= 0); return s; }
+    string[] seps = { " ", "\t", };
+    string[] ReadSplit() => ReadLineUntil().Split(seps, StringSplitOptions.RemoveEmptyEntries);
+    T[] ReadArray<T>(Func<string, T> f) => ReadSplit().Select(f).ToArray();
+    T Read1<T>(Func<string, T> f) => f(ReadLineUntil());
+    (T, T) Read2<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1]); }
+    (T, T, T) Read3<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1], s[2]); }
+
+    void Solve()
+    {
+
+        var a = new int[4];
+        for (int i = 0; i < 4; ++i)
+            a[i] = Read1(int.Parse);
+        static bool is89(int x) => x == 8 || x == 9;
+        if (is89(a[0]) && is89(a[3]) && a[1] == a[2])
+            sw.WriteLine("ignore");
+        else
+            sw.WriteLine("answer");
+
+        sw.Flush();
+    }
+}
