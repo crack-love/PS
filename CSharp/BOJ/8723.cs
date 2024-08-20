fab70f4d1451153896b147c15ddc91b6c25f6e4d
KYG_Ideapad <masterchip@naver.com>
2024-08-20 14:32:18 +0900

    8723

---
+++ CSharp/BOJ/8723.cs
@@ -0,0 +1,38 @@
+﻿namespace BOJ;
+class P8723
+{
+    static void Main0() => new P8723().Solve();
+    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1, 0 };
+    int[] dy = { -1, 1, 0, 0, -1, 1, 1, -1, 0 };
+    bool Step(int x, int y, int r, int c) => x < 0 || x >= r || y < 0 || y >= c;
+    string ReadLineUntil() { string s; do { s = sr.ReadLine(); } while (s.Length <= 0); return s; }
+    string[] seps = { " ", "\t", };
+    string[] ReadSplit() => ReadLineUntil().Split(seps, StringSplitOptions.RemoveEmptyEntries);
+    T[] ReadArray<T>(Func<string, T> f) => ReadSplit().Select(f).ToArray();
+    T Read1<T>(Func<string, T> f) => f(ReadLineUntil());
+    (T, T) Read2<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1]); }
+    (T, T, T) Read3<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1], s[2]); }
+
+    void Solve()
+    {
+        var (a, b, c) = Read3(int.Parse);
+
+        int ans;
+
+        if (a == b && b == c)
+            ans = 2;
+        else if (Math.Sqrt(a * a + b * b) == c)
+            ans = 1;
+        else if (Math.Sqrt(a * a + c * c) == b)
+            ans = 1;
+        else if (Math.Sqrt(b * b + c * c) == a)
+            ans = 1;
+        else
+            ans = 0;
+
+        sw.WriteLine(ans);
+        sw.Flush();
+    }
+}
