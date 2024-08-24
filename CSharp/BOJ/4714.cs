bd3cc7e4d6fb5796a143e974bf07f381edc0c408
KYG_Ideapad <masterchip@naver.com>
2024-08-24 15:02:26 +0900

    4714

---
+++ CSharp/BOJ/4714.cs
@@ -0,0 +1,33 @@
+﻿namespace BOJ;
+class P4714
+{
+    static void Main0() => new P4714().Solve();
+    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1, 0 };
+    int[] dy = { -1, 1, 0, 0, -1, 1, 1, -1, 0 };
+    bool Step(int x, int y, int r, int c) => x < 0 || x >= r || y < 0 || y >= c;
+    string ReadLineUntil() { string s; do { s = sr.ReadLine(); } while (s.Length <= 0); return s; }
+    string[] seps = { " ", "\t", };
+    string[] ReadSplit() => ReadLineUntil().Split(seps, StringSplitOptions.RemoveEmptyEntries);
+    T[] ReadArray<T>(Func<string, T> f) => ReadSplit().Select(f).ToArray();
+    T Read1<T>(Func<string, T> f) => f(ReadLineUntil());
+    (T, T) Read2<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1]); }
+    (T, T, T) Read3<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1], s[2]); }
+
+    void Solve()
+    {
+        while (true)
+        {
+            var f = Read1(double.Parse);
+            if (f == -1.0)
+            {
+                break;
+            }
+            sw.WriteLine($"Objects weighing {f:F2} on Earth will weigh {f * 0.167:F2} on the moon.");
+
+        }
+
+        sw.Flush();
+    }
+}
