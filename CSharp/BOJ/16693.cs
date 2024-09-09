81d860aaf8367cd8ba291ed3ffbf9f1a0bd5d56e
KYG_Ideapad <masterchip@naver.com>
2024-09-09 18:54:03 +0900

    16693

---
+++ CSharp/BOJ/16693.cs
@@ -0,0 +1,36 @@
+﻿namespace BOJ;
+class P16693
+{
+    static void Main0() => new P16693().Solve();
+    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1, 0 };
+    int[] dy = { -1, 1, 0, 0, -1, 1, 1, -1, 0 };
+    bool Step(int x, int y, int r, int c) => x < 0 || x >= r || y < 0 || y >= c;
+    string ReadLineUntil() { string s; do { s = sr.ReadLine(); } while (s.Length <= 0); return s; }
+    string[] seps = { " ", "\t", };
+    string[] ReadSplit() => ReadLineUntil().Split(seps, StringSplitOptions.RemoveEmptyEntries);
+    T[] ReadArray<T>(Func<string, T> f) => ReadSplit().Select(f).ToArray();
+    T Read1<T>(Func<string, T> f) => f(ReadLineUntil());
+    (T, T) Read2<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1]); }
+    (T, T, T) Read3<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1], s[2]); }
+
+    void Solve()
+    {
+        var (a1, p1) = Read2(int.Parse);
+        var (r1, p2) = Read2(int.Parse);
+
+        var app1 = (double)a1 / p1;
+        var app2 = r1 * r1 * Math.PI / p2;
+        if (app1 > app2)
+        {
+            sw.WriteLine("Slice of pizza");
+        }
+        else
+        {
+            sw.WriteLine("Whole pizza");
+        }
+
+        sw.Flush();
+    }
+}
