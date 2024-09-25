2f6bcb38be8d13f2ea22c548284c21793724ce58
KYG_Ideapad <masterchip@naver.com>
2024-09-25 15:59:38 +0900

    20673

---
+++ CSharp/BOJ/20673.cs
@@ -0,0 +1,35 @@
+﻿namespace BOJ;
+class P20673
+{
+    static void Main0() => new P20673().Solve();
+    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1, 0 };
+    int[] dy = { -1, 1, 0, 0, -1, 1, 1, -1, 0 };
+    bool Step(int x, int y, int r, int c) => x < 0 || x >= r || y < 0 || y >= c;
+    string ReadLineUntil() { string s; do { s = sr.ReadLine(); } while (s.Length <= 0); return s; }
+    string[] seps = { " ", "\t", };
+    string[] ReadSplit() => ReadLineUntil().Split(seps, StringSplitOptions.RemoveEmptyEntries);
+    T[] ReadArray<T>(Func<string, T> f) => ReadSplit().Select(f).ToArray();
+    T Read1<T>(Func<string, T> f) => f(ReadLineUntil());
+    (T, T) Read2<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1]); }
+    (T, T, T) Read3<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1], s[2]); }
+
+    void Solve()
+    {
+        // 50 / 1m, 10/1m - white
+        //          30/1m - red
+
+        var p = Read1(int.Parse);
+        var q = Read1(int.Parse);
+
+        if (p <= 50 && q <= 10)
+            sw.WriteLine("White");
+        else if (q > 30)
+            sw.WriteLine("Red");
+        else
+            sw.WriteLine("Yellow");
+
+        sw.Flush();
+    }
+}
