afe2434cfd9107a09406df3976996f91c5a4852e
KYG_Ideapad <masterchip@naver.com>
2024-09-28 17:30:29 +0900

    30402

---
+++ CSharp/BOJ/30402.cs
@@ -0,0 +1,41 @@
+﻿namespace BOJ;
+class P30402
+{
+    static void Main0() => new P30402().Solve();
+    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1, 0 };
+    int[] dy = { -1, 1, 0, 0, -1, 1, 1, -1, 0 };
+    bool Step(int x, int y, int r, int c) => x < 0 || x >= r || y < 0 || y >= c;
+    string ReadLineUntil() { string s; do { s = sr.ReadLine(); } while (s.Length <= 0); return s; }
+    string[] seps = { " ", "\t", };
+    string[] ReadSplit() => ReadLineUntil().Split(seps, StringSplitOptions.RemoveEmptyEntries);
+    T[] ReadArray<T>(Func<string, T> f) => ReadSplit().Select(f).ToArray();
+    T Read1<T>(Func<string, T> f) => f(ReadLineUntil());
+    (T, T) Read2<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1]); }
+    (T, T, T) Read3<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1], s[2]); }
+
+    void Solve()
+    {
+        for(int i=0;i<15;++i)
+        {
+            var s = ReadLineUntil();
+            if (s.Contains('w'))
+            {
+                sw.WriteLine("chunbae");
+                break;
+            }
+            else if (s.Contains('b'))
+            {
+                sw.WriteLine("nabi");
+                break;
+            }
+            else if (s.Contains('g'))
+            {
+                sw.WriteLine("yeongcheol");
+                break;
+            }
+        }
+        sw.Flush();
+    }
+}
