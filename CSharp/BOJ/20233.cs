ce2125236a93bb4858b1220569d812f92a6073e4
KYG_Ideapad <masterchip@naver.com>
2024-10-09 18:09:13 +0900

    20233

---
+++ CSharp/BOJ/20233.cs
@@ -0,0 +1,32 @@
+﻿namespace BOJ;
+class P20233
+{
+    static void Main0() => new P20233().Solve();
+    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1, 0 };
+    int[] dy = { -1, 1, 0, 0, -1, 1, 1, -1, 0 };
+    bool Step(int x, int y, int r, int c) => x < 0 || x >= r || y < 0 || y >= c;
+    string ReadLineUntil() { string s; do { s = sr.ReadLine(); } while (s.Length <= 0); return s; }
+    string[] seps = { " ", "\t", };
+    string[] ReadSplit() => ReadLineUntil().Split(seps, StringSplitOptions.RemoveEmptyEntries);
+    T[] ReadArray<T>(Func<string, T> f) => ReadSplit().Select(f).ToArray();
+    T Read1<T>(Func<string, T> f) => f(ReadLineUntil());
+    (T, T) Read2<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1]); }
+    (T, T, T) Read3<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1], s[2]); }
+
+    void Solve()
+    {
+        var a = Read1(int.Parse); // 30
+        var x = Read1(int.Parse);
+        var b = Read1(int.Parse); // 45
+        var y = Read1(int.Parse);
+        var t = Read1(int.Parse);
+        var totalmin = t * 21;
+
+        var ac = a + Math.Max(0, t - 30) * x * 21;
+        var bc = b + Math.Max(0, t - 45) * y * 21;
+        sw.WriteLine($"{ac} {bc}");
+        sw.Flush();
+    }
+}
