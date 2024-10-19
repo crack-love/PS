48e60c01e58c77f0d1cb37262a1b2fd054a14c25
KYG_Ideapad <masterchip@naver.com>
2024-10-19 20:41:02 +0900

    30468

---
+++ CSharp/BOJ/30468.cs
@@ -0,0 +1,28 @@
+﻿namespace BOJ;
+class P30468
+{
+    static void Main() => new P30468().Solve();
+    StreamReader sr = new(Console.OpenStandardInput(), bufferSize: 102400);
+    StreamWriter sw = new(Console.OpenStandardOutput(), bufferSize: 102400);
+    int[] dx = { 0, 0, -1, 1, -1, -1, 1, 1, 0 };
+    int[] dy = { -1, 1, 0, 0, -1, 1, 1, -1, 0 };
+    bool Step(int x, int y, int r, int c) => x < 0 || x >= r || y < 0 || y >= c;
+    string ReadLineUntil() { string s; do { s = sr.ReadLine(); } while (s.Length <= 0); return s; }
+    string[] seps = { " ", "\t", };
+    string[] ReadSplit() => ReadLineUntil().Split(seps, StringSplitOptions.RemoveEmptyEntries);
+    T[] ReadArray<T>(Func<string, T> f) => ReadSplit().Select(f).ToArray();
+    T Read1<T>(Func<string, T> f) => f(ReadLineUntil());
+    (T, T) Read2<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1]); }
+    (T, T, T) Read3<T>(Func<string, T> f) { var s = ReadArray(f); return (s[0], s[1], s[2]); }
+
+    void Solve()
+    {
+        var a = ReadArray(int.Parse);
+        var n = a[^1];
+        var des = n * (a.Length - 1);
+        var sum = a.Sum() - n;
+        var ans = Math.Max(0, des - sum);
+        sw.WriteLine(ans);
+        sw.Flush();
+    }
+}
